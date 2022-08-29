52b68f748f04e7ecc39ac065651a6b8f4a840bce
Biron Christopher <96913467+kyslegion@users.noreply.github.com>
2022-08-29 22:33:40 +0200

    Add files via upload

--- Node.js/Express/index.js
+++ Node.js/Express/index.js
@@ -7,8 +7,10 @@ app.use(express.urlencoded({ extended: false }));
 app.use(express.static(__dirname+ '/'));
 
 app.get('/',(req,res)=>{
+    console.log(req.route.path);
     res.sendFile(path.join(__dirname, './connexion.html'));
 });
+// console.log(x.route);
 let server=app.listen(3000, ()=>{
     console.log("Serveur localhost:", server.address().port);
 });
